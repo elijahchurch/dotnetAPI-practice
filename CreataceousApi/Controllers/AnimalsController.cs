da0225f3aa3c0b5128b0a1892698f3eb3612d138
Jason E. Church <elijahchurch@gmail.com>
2023-08-13 14:52:26 -0700

    Create Put and Delete routes for Animal Controller

--- CreataceousApi/Controllers/AnimalsController.cs
+++ CreataceousApi/Controllers/AnimalsController.cs
@@ -6,7 +6,7 @@ namespace CretaceousApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AnimalsController: ControllerBase
+    public class AnimalsController : ControllerBase
     {
         private readonly CretaceousApiContext _db;
 
@@ -39,7 +39,55 @@ namespace CretaceousApi.Controllers
         {
             _db.Animals.Add(animal);
             await _db.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId}, animal);
+            return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+        }
+
+        //Put :api/Animals/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _db.Animals.Update(animal);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _db.Animals.Any(e => e.AnimalId == id);
+        }
+
+        //Delete: api/Animals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            Animal animal = await _db.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            _db.Animals.Remove(animal);
+            await _db.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
